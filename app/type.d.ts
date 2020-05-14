a8b45de2ec916eb23427d1514380b8f82c1c0caf
lplesko <57364521+lplesko@users.noreply.github.com>
2020-05-14 17:04:43 -0400

    Update type.d.ts

--- app/type.d.ts
+++ app/type.d.ts
@@ -1,5 +1,6 @@
 interface StatisticsResponse {
    month: "January" | "February" | "March" | "April" | "May" | "June" | "July" | "August" | "September" | "October" | "November" | "December" | string,
+   year: "2016" | "2017" | "2018" | "2019" | string,
    value: number
  } 
   
