def2fd6f18b7d7ab2bff72f38a96f4cf1e24841a
lplesko <57364521+lplesko@users.noreply.github.com>
2020-05-19 20:56:04 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -214,7 +214,7 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                     districtsLayer = new FeatureLayer({
                         title: "districts",
                         portalItem: {
-                            id: "c1c22edd96a4477ba505e222e176ba80"
+                            id: "3a8aae65f6d64c9dacce3049ebe32f0c"
                         },
                         popupTemplate: null,
                         opacity: 0,
